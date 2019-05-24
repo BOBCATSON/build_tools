285f7031f3ef005071da24d213b7e04c4bb4e477
BOBCATSON <robjcatlin@gmail.com>
2019-05-24 21:20:56 +0100

    pug, scss and es6 compiling with browser refresh on save

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var pug = require('gulp-pug');
+var babel = require('gulp-babel');
 var browserSync = require('browser-sync').create();
 
 
@@ -9,14 +10,14 @@ var browserSync = require('browser-sync').create();
 //compile scss into css
 function sassCompile() {
   return gulp.src('./app/scss/**/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./app/css'))
-  .pipe(browserSync.stream())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app/css'))
+    .pipe(browserSync.stream())
 }
 
 //compile pug into HTML
 function pugCompile() {
-  return gulp.src('./app/pug/*.pug')
+  return gulp.src('./app/pug/**/*.pug')
     .pipe(pug({
      doctype: 'html',
      pretty: true
@@ -27,6 +28,15 @@ function pugCompile() {
    }))
 }
 
+//compile ES6 into ES5
+function es6Compile() {
+  return gulp.src('./app/es6/**/*.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(gulp.dest('./app/js'))
+}
+
 
 function watch() {
   browserSync.init({
@@ -35,12 +45,14 @@ function watch() {
     }
   });
 
-  gulp.watch('./app/scss/styles.scss', sassCompile)
-  gulp.watch('./app/pug/*.pug', pugCompile)
-  gulp.watch('./app/js/**/*.js').on('change', browserSync.reload)
+  gulp.watch('./app/scss/**/*.scss', sassCompile)
+  gulp.watch('./app/pug/**/*.pug', pugCompile)
+  gulp.watch('./app/es6/**/*.js', es6Compile)
+  // gulp.watch('./app/js/**/*.js').on('change', browserSync.reload)
   gulp.watch('./app/*.html').on('change', browserSync.reload)
 }
 
 exports.sassCompile = sassCompile;
 exports.pugCompile = pugCompile;
+exports.es6Compile = es6Compile;
 exports.watch = watch;
